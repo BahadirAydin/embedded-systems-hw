f3cfcacd6efeb8d68422cd7d35cfce4776ab32fb
BahadirAydin <bahadiraydin1923@gmail.com>
2024-04-21 16:15:11 +0300

    up

--- the2/the2.X/main.c
+++ the2/the2.X/main.c
@@ -95,7 +95,7 @@ typedef struct {
 byte activePieceGrid[4];
 byte submittedGrid[4];
 byte submit = 1;
-byte portBPins = 0;
+byte portBPins = PORTB;
 byte rotationFlag = 0;
 byte currentPiece = 0; // 0 for dot, 1 for square, 2 for L
 
@@ -217,7 +217,7 @@ void init() {
     initializeInterrupts();
     initializeTimers();
     initializePorts();
-    spawnShape(0);
+    spawnShape(currentPiece);
     incrementCurrentPiece();
     printGrid();
 }
